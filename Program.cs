7c1f8ced9d2e9ed3d5336425ee521c53a916c5ad
jack <jack.goldsbrough@outlook.com>
2025-08-27 12:52:47 +0200

    added easier data set and proper wrong count

--- Program.cs
+++ Program.cs
@@ -7,8 +7,9 @@ class Program
 		static void Main(string[] args)
 		{
 			Console.WriteLine("\n*** Welcome to the game... test your k-knowledge about Linux Console Commandes ***");
-			Console.WriteLine("You will be asked how to give a command line in Linux, respond entring the command\n");
-			
+			Console.WriteLine("You will be asked how to give a command line in Linux, respond entring the command");
+			Console.WriteLine("3 Chances - then the answer will be given\n");
+
 			string fileName = args[0];
 			String[] lines = File.ReadAllLines(fileName);
 			int length = lines.Length;
@@ -17,7 +18,7 @@ class Program
 			int[] questionNumbers = Enumerable.Range(0,length)
 							  .OrderBy(x => rnd.Next())
 							  .ToArray();
-				
+					
 			Console.WriteLine("Ready?? [y/n] ");
 			
 			string option = Console.ReadLine().ToLower();
@@ -38,16 +39,33 @@ class Program
 					string[] qNa = lines[questionNumbers[n-1]].Split(',');
 					correct = false;
 						
-
+					int count = 3;
 
 					Console.WriteLine(qNa[0]);
-					while(!correct)
-					{	string answer = Console.ReadLine();
+					while(!correct)					
+					{	if (count < 1) 
+						{
+							Console.WriteLine($"Correct answer was: {qNa[1]}");
+							break;
+						}
+						else
+						{
+
+							string answer = Console.ReadLine();
 						
-						if (answer == qNa[1]) correct = true;
-						else Console.WriteLine("Incorrect\n ");
+							if (answer == qNa[1])
+							{
+								correct = true;
+								Console.WriteLine("Correct!!");
+							}
+							else 
+							{
+								Console.WriteLine("Incorrect\n ");
+								count--;
+							}
+						}
 					}
-					Console.WriteLine("Correct!!\n\nNext Question:");
+					Console.WriteLine("\n\nNext Question:");
 				
 					n--;		
 				}
